85f9681020337165897b731824f80d739bf677af
Charley Liao <c24liao@gmail.com>
2024-04-18 21:24:11 -0700

    Initial Code_File Commit

---
+++ Code_File/README.md
@@ -0,0 +1,31 @@
+## Final Exam ACIT 2450
+
+#### Exam Weight:
+
+50% MC, 50% Weighted
+
+#### From Midterm:
+
+Coffee supply question (given one file) and do stuff based on that file
+
+### Final Exam will be:
+
+Using 2 files at the same time
+
+-   The data is split between 2 files
+-   Meaning you will need to do things twice to call the files
+
+#### How to do this
+
+The two files are "csv-ish" not JSON or txt files. Be familiar on how to PARSE CSV files or JSON Files.
+
+-   **This special file is a in-between of CSV and JSON**
+-   Once you parse the CSV, you will get a JSON file
+    -   JSON.parse() will be used
+
+### Prompt will be similar to...
+
+The file format will be using id checking, and is not in a database, but in two file one file may be called USER and the other HOBBIES
+
+-   Questions will be asking for a specific criteria and you need to index the value of the criteria from the two files
+-   Either promises or async await, not callback functions
---
+++ Code_File/app.js
@@ -0,0 +1,100 @@
+const fs = require("fs/promises");
+const { EOL } = require("os");
+
+function title(str) {
+    return str
+        .toLowerCase()
+        .split(" ")
+        .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+        .join(" ");
+}
+
+// * Question 1 - Who is the oldest user?
+function question1(data) {
+    const userRows = data.split(EOL);
+    const oldestUser = { name: "", age: -1 };
+    const youngestUser = { name: "", age: Infinity };
+    userRows.forEach((row) => {
+        const [id, name, age, hobby] = row.split(",");
+        if (parseInt(age) > oldestUser.age) {
+            oldestUser.name = name;
+            oldestUser.age = age;
+        }
+        if (parseInt(age) < youngestUser.age) {
+            youngestUser.name = name;
+            youngestUser.age = age;
+        }
+    });
+    console.log(`The oldest user is ${title(oldestUser.name)} of age ${oldestUser.age}`);
+    console.log(`The youngest user is ${title(youngestUser.name)} of age ${youngestUser.age}`);
+}
+
+// * Question 2 - Who are the top 5 oldest users?
+// * Will have to use "sort" method (Quick Sort)
+function question2(data) {
+    // ! Sort the users by age
+    const userRows = data.split(EOL);
+    const users = [];
+    userRows.forEach((row) => {
+        const [id, name, age, hobby] = row.split(",");
+        users.push({ id, name, age, hobby });
+    });
+    users.sort((a, b) => a.age - b.age);
+    console.log("Top 5 Youngest Users");
+    users.slice(0, 5).forEach((user) => {
+        console.log(user);
+        // console.log(`${title(user.name)} is ${user.age} years old`);
+    });
+    console.log("=====================================");
+    console.log("Top 5 Oldest Users");
+    users.slice(-5).forEach((user) => {
+        // console.log(`${title(user.name)} is ${user.age} years old`);
+        console.log(user);
+    });
+}
+
+// * Question 3 - What is the most popular hobbies among the users?
+// * They will be using another file, so you will need to read the file and parse it
+function question3(userData, hobbyData) {
+    const userRows = userData.split(EOL);
+    const hobbyRows = hobbyData.split(EOL);
+    const hobbyCount = {};
+    const hobbyInfo = {};
+
+    userRows.forEach((user) => {
+        const [id, name, age, hobby] = user.split(",");
+        hobbyCount[hobby] = hobbyCount[hobby] ? hobbyCount[hobby] + 1 : 1;
+    });
+    hobbyRows.forEach((hobby) => {
+        const [id, name] = hobby.split(",");
+        hobbyInfo[id] = name;
+    });
+    const hobbyID = Object.keys(hobbyCount).reduce((a, b) => (hobbyCount[a] > hobbyCount[b] ? a : b));
+    const hobbyName = hobbyInfo[Object.keys(hobbyCount).reduce((a, b) => (hobbyCount[a] > hobbyCount[b] ? a : b))];
+    // const hobbyName = hobbyRows.find((hobby) => hobby.split(",")[0] === hobbyInfo).split(",")[1];
+    console.log(`${title(hobbyName)} is the most popular hobby with ${hobbyCount[hobbyID]} users`);
+}
+
+// Might be using a map to get the hobbies of the users
+
+// * Question 4 
+// ! HINT: They will be using both files, so you will need to read both files and parse them
+
+async function main() {
+    // ! Less Correct Way
+    // const users = await fs.readFile("users.csv", "utf-8");
+    // const hobbies = await fs.readFile("hobbies.csv", "utf-8");
+    try {
+        // ! More Correct Way; Runs the csv files in parallel
+        const [userData, hobbyData] = await Promise.all([fs.readFile("users.csv", "utf-8"), fs.readFile("hobbies.csv", "utf-8")]);
+        // * Returns the result of array [Users.csv Content, Hobbies.csv Content] ; using result[1] will get Users.csv Content
+        question1(userData);
+        question2(userData);
+        question3(userData, hobbyData);
+        // question4();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
